1f748ebf0cb95515eb0f1d2b38e473280b786b69
António Afonso <antonio.afonso@gmail.com>
2012-04-27 13:36:57 -0700

    Add TreeViewer component

---
+++ examples/components/type-converter.js
@@ -0,0 +1,17 @@
+/* <copyright>
+ This file contains proprietary software owned by Motorola Mobility, Inc.<br/>
+ No rights, expressed or implied, whatsoever to this software are provided by Motorola Mobility, Inc. hereunder.<br/>
+ (c) Copyright 2012 Motorola Mobility, Inc.  All Rights Reserved.
+ </copyright> */
+var Montage = require("montage").Montage;
+var Converter = require('montage/core/converter/converter').Converter;
+
+exports.TypeConverter = Montage.create(Converter, {
+    convert: {value: function(value) {
+        if (typeof value.children === "undefined") {
+            return "leaf";
+        } else {
+            return "branch";
+        }
+    }}
+});
